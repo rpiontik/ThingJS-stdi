7208140fb3494824e2e5d7969efe4b5ff11b25e4
R.Piontik <r.piontik@mail.ru>
2019-10-21 22:35:12 +0300

    Added bit_port interface

---
+++ implementation/tgsi_bit_port.h
@@ -0,0 +1,11 @@
+/*
+ *  Created on: 21 мар. 2019 г.
+ *      Author: rpiontik
+ */
+
+#ifndef STDI_BIT_PORT_H_
+#define STDI_BIT_PORT_H_
+
+void thingjsBitPortRegister(void);
+
+#endif /* STDI_1WIRE_H_ */
